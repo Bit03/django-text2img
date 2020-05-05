82b90c9e5f60f40831c7914df6a4a765c2cd6528
jiaxin <edison7500@gmail.com>
2020-05-05 18:07:13 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,13 @@ install_requires = (["Django >= 1.11", "Pillow >= 5.0"],)
 
 setup(
     name="django-text2img",
+    author="Chainnews",
+    author_email="hi@chainnews.com",
     version=text2img.__version__,
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
+    keywords=["django", "news2img", "pillow"]
 )
+
+
